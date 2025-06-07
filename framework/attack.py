561a6601b496ff3fa1b03b51df3d12475e2f98c3
William Dennis <wwdennis.home@gmail.com>
2025-06-07 15:48:56 +0100

    Adding first iteration of class structure and have added working requirements

--- framework/attack.py
+++ framework/attack.py
@@ -1,14 +1,89 @@
 import numpy as np
-import pandas as pd
 import torch
 from framework.legacy.tools import bitflip_float32
 
-def attack(trained_model, X, y, bit_i, device):
-    trained_model.to(device)
+class injector():
+    def __init__(self, trained_model, X, y, criterion, device = None):
+        """
+        Initlaise the injector
+
+        - [ ] Check that the model is compatitble
+        - [x] Check that the X can do a forward pass
+        - [x] Check that the result of the forward pass gives a result with y and criterion
+        - [ ] move model and data into the correct device
+        - [ ] allows other floating point architectures
+        """
+
+        if device is None:
+            if torch.cuda.is_available():
+                self.device = "cuda"
+            else:
+                self.device = "cpu"
+        else:
+            self.device = device
+
+        # assert that the model and inputs are in float 32
+
+        self.model = trained_model
+        self.model.eval()
+        self.model.to(device)
+        if isinstance(X, torch.Tensor):
+            self.X = X.clone().detach().to(device=device, dtype=torch.float32)
+        else:
+            self.X = torch.tensor(X, dtype=torch.float32, device=device)
+
+        print(f"Testing a forward pass on {self.device}...")
+        y_pred = self.model(self.X)
+
+        self.baseline_score = criterion(y, y_pred)
+        print("Basline Criterion Score:", self.baseline_score)
+        
+        self.criterion = criterion
+        self.y = y
+
+    def run_seu(self, bit_i):
+        """Perform a bitflip at index i across every variable in the nn"""
+        self.model.eval()
+
+        results = {
+            "tensor_location": [],
+            "criterion_score": [],
+            "layer_name": [],
+            "value_before": [],
+            "value_after": []
+        }
+
+        with torch.no_grad(): # disable tracking gradients 
+            # iterate though each layer of the nn
+            for layer_name, tensor in self.model.named_parameters():
+                original_tensor = tensor.data.clone() # copy original tensor values
+                tensor_cpu = original_tensor.cpu().numpy() # move to cpu for iteration of indexes
+
+                for idx in np.ndindex(tensor_cpu.shape):
+                    original_val = tensor_cpu[idx]
+                    seu_val = bitflip_float32(original_val, bit_i) # perform bitfliip
+
+                    tensor.data[idx] = torch.tensor(seu_val, device = self.device, dtype=tensor.dtype)
+                    criterion_score = self.criterion(self.y, self.model(self.X))
+                    tensor.data[idx] = original_tensor[idx]
+
+                    results["tensor_location"].append(idx)
+                    results["criterion_score"].append(criterion_score)
+                    results["layer_name"].append(layer_name)
+                    results["value_before"].append(original_val)
+                    results["value_after"].append(seu_val)
+
+        return results
+
+
+def attack(trained_model, X, y, bit_i):
+
+
+    trained_model
     trained_model.eval()
 
     # Prepare inputs as torch tensors on the correct device
-    X_tensor = torch.tensor(X, dtype=torch.float32, device=device)
+    X_tensor = torch.tensor(X, dtype=torch.float32, device = "cpu")
 
     idxs = []
     accs = []
