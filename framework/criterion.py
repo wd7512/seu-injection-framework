2211483246aed2ea2d2d84508e347ecd490b8b2c
William Dennis <wwdennis.home@gmail.com>
2025-06-11 19:15:20 +0100

    Adding new classification function

--- framework/criterion.py
+++ framework/criterion.py
@@ -1,15 +1,21 @@
 import numpy as np
 from sklearn.metrics import accuracy_score
 
-def binary_classifcation_accuracy(y_true, model_output):
-    unique_bins = np.unique(y_true)
-    assert np.unique(y_true).size == 2
+def classification_accuracy(model, X_tensor, y_true, device = None):
 
-    lo, hi = unique_bins[0], unique_bins[1]
+    if device:
+        X_tensor.to(device)
+        model.to(device)
 
-    y_bool = model_output > ((lo + hi) / 2)
-    y_pred = np.zeros_like(y_true)
-    y_pred[y_bool] = hi
-    y_pred[~y_bool] = lo
+    model.eval()
 
+    y_pred = model(X_tensor)
+    return multiclass_classification_accuracy(y_true, y_pred)
+
+def multiclass_classification_accuracy(y_true, model_output):
+    """
+    y_true: array of shape (N,) with integer class labels [0..9]
+    model_output: array of shape (N, 10) with predicted scores (logits or probabilities)
+    """
+    y_pred = np.argmax(model_output, axis=1)
     return accuracy_score(y_true=y_true, y_pred=y_pred)
